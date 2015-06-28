07f1ed45478655bfd38e241a716a80ddf4823cf4
Kevin Minehart <kmineh0151@gmail.com>
2015-06-28 17:12:03 -0500

    Added Component.cs

---
+++ Lime/Component.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lime
+{
+    class Component
+    {
+        /// <summary>
+        /// If the component is enabled, then it will be updated in the game loop.
+        /// </summary>
+        public bool Enabled
+        {
+            get
+            {
+                return Enabled;
+            }
+            set
+            {
+                this.Enabled = value;
+            }
+        }
+
+        /// <summary>
+        /// The assocated GameObject.
+        /// This GameObject will have this Component as a member.
+        /// </summary>
+        public GameObject GameObject
+        {
+            get
+            {
+                return this.GameObject;
+            }
+            set
+            {
+                this.GameObject = value;
+            }
+        }
+
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Destroy()
+        {
+
+        }
+
+        public void OnEnable()
+        {
+
+        }
+
+        public void OnDisable()
+        {
+
+        }
+
+        public void OnDestroy()
+        {
+
+        }
+    }
+}
