f782371ad602ccf8f8f00346078ca83feeb8e299
Kevin Minehart <kmineh0151@gmail.com>
2015-06-28 15:46:18 -0500

    renamed Game1.cs to Lime.cs

--- Lime/Program.cs
+++ Lime/Program.cs
@@ -14,7 +14,7 @@ namespace Lime
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Lime())
                 game.Run();
         }
     }
